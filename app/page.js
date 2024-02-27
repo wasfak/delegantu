5fa9b05817b70851c1a66842738e5e022bb7d496
wasfak <hardmodz@yahoo.com>
2024-02-27 15:19:06 +0200

    last 2

--- app/page.js
+++ app/page.js
@@ -4,10 +4,5 @@ import { getUserById } from "@/lib/actions/userActions";
 import User from "@/models/userModel";
 
 export default async function Home() {
-  const user = await User.findOne({
-    clerkId: "user_2cx0vOIbOrb2SR22PXrsVglmP2n",
-  });
-
-  console.log(user);
   return <h1>home page</h1>;
 }
